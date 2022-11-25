92f4a8532bdf66692f002554664057db9f5c448a
Nana <nanaolomaiyani@gmail.com>
2022-11-26 00:35:24 +0300

    commit tree 7b93a5009b4e15ec56c9f1b2d799bd3d3c5d4a05

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,4 @@
 class PostsController < ApplicationController
-
   def show
     post = Post.find(params[:id])
     
@@ -11,13 +10,15 @@ class PostsController < ApplicationController
 
     post.update(post_params)
 
+    post.update!(post_params)
     render json: post
+  rescue ActiveRecord::RecordInvalid => invalid 
+    render json: {errors: invalid.record.errors}, status: :unprocessable_entity
   end
 
   private
-
+  
   def post_params
     params.permit(:category, :content, :title)
   end
-
 end
